eee0d5bd8f0f91eb28a64cff2330397144733123
Deepak <deepak@itinfonity.com>
2026-02-18 05:36:45 +0000

    reconfigured routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AppRoutes from "./components/routes/AppRoutes";
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
+    <BrowserRouter basename="/authorswap-frontend">
+        <AppRoutes />
     </BrowserRouter>
   );
 }
