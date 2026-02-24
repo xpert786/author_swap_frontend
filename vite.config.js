2d8f6675f0967422f569ebf165d08eba596e7b91
Shivam <srivastva2006@gmail.com>
2026-02-24 11:29:11 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/authorswap-frontend/",
   plugins: [react(), tailwindcss()],
 });
