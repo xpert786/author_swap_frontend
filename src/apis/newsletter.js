14fd078e0c9465bcf52dd424712f484d19942381
Arun Rana <m.xpert786@gmail.com>
2026-02-26 18:12:20 +0530

    newsletter

--- src/apis/newsletter.js
+++ src/apis/newsletter.js
@@ -2,20 +2,20 @@ import apiClient from "./client";
 
 export const createNewsSlot = async (formData) => {
     return apiClient.post("newsletter-slot/", formData);
-} 
+}
 
 export const getNewsSlot = async () => {
     return apiClient.get("newsletter-slot/");
-} 
+}
 
 export const deleteNewsSlot = async (id) => {
     return apiClient.delete(`newsletter-slot/${id}/`);
-} 
+}
 
 export const updateNewsSlot = async (id, formData) => {
     return apiClient.patch(`newsletter-slot/${id}/`, formData);
-} 
+}
 
-export const statsNewsSlot = async () => {
-    return apiClient.get(`newsletter-stats/`);
-} 
+export const statsNewsSlot = async (params) => {
+    return apiClient.get(`newsletter-stats/`, { params });
+}
